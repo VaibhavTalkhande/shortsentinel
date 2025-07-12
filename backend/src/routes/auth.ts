bf26255f4cc09eb6633cbdb0629aef53b8e52d47
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-07-13 00:03:56 +0530

    /me

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -49,7 +49,7 @@ router.get("/me", async (req, res) => {
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   try {
-    const userId = verifyToken(token);
+    const {userId} = verifyToken(token);
     const user = await prisma.user.findUnique({ where: { id: userId } });
     if (!user) return res.status(404).json({ message: "User not found" });
 
